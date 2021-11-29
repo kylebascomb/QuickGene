77a88463506fb21ee031e22c9176a93b631418ec
Kyle <kylebascomb@gmail.com>
2021-11-29 09:44:30 -0800

    Added length to basic description

--- sequences.py
+++ sequences.py
@@ -37,22 +37,7 @@ def format_seq(seq_record):
         'sequence': str(seq_record.seq.upper())
     }
 
-def write_fasta_file(formatted_sequence, file_path='./'):
-    '''
-    This function formats the information in the formatted_sequence dictionary and
-    writes it to a file.
-    Parameters:
-        formatted_sequence (dict) : Dictionary of the sequence information
-        filepath (str) : Relative file path to write to. The default filepath is the current directory     
-    '''
-    header = '>{} | {} | {}'.format(formatted_sequence['id'], formatted_sequence['name'], formatted_sequence['description'])
-    if file_path == './':
-        file_path = file_path + formatted_sequence['name'] + '.fasta'
-    
-    with open(file_path, 'w') as f:
-        f.write(header)
-        f.write('\n')
-        f.write(formatted_sequence['sequence'])
+
 
         
 
